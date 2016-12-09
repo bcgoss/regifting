09d8820eb4f81d5503ee1696d4b0f6528ce86ef6
Jennifer <jennifersoden@hotmail.com>
2016-12-08 19:40:08 -0700

    Create crud for gift

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,2 +1,21 @@
 class GiftsController < ApplicationController
+  def new
+    @gift = Gift.new
+  end
+
+  def create
+    @gift = Gift.new(gift_params)
+    @gift.user_id = current_user.id if current_user
+    if @gift.save
+      flash[:success] = "Gift was created."
+    else
+      flash[:warning] = @gift.errors.full_messages.join(", ")
+    end
+    redirect_to gifts_path
+  end
+
+  private
+    def gift_params
+      params.require(:gift).permit(:name, :description, :image_url)
+    end
 end
